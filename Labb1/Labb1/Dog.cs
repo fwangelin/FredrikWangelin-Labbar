32de8de048eea7c44c826584ea49461886c081fb
Fredrik Wängelin <f.wangelin@gmail.com>
2016-10-12 13:55:50 +0200

    intial comit changes

---
+++ Labb1/Labb1/Dog.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb1
+{
+    class Dog
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Breed { get; set; }
+
+        public Dog()
+        {
+        }
+
+        public Dog(string name, int age, string breed)
+        {
+            Name = name;
+            Age = age;
+            Breed = breed;
+        }
+        public string Introduction()
+        {
+            return String.Format("Dogs name is {0}, it's age is {1} and the breed is {2}",
+                                 Name, Age, Breed);
+
+        }
+
+    }
+}
---
+++ Labb1/Labb1/runtime.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb1
+{
+    class Runtime
+    {
+        public void Start()
+        {
+            #region Step One
+            //  This was step 1, but I put them directly in "List" for step 2...
+            //Dog dog1 = new Dog
+            //{
+            //    Name = "Little Helper",
+            //    Age = 3,
+            //    Breed = "Golden Retreiver"
+            //};
+            //Dog dog2 = new Dog("Rudolf", 4, "Bulldog");
+            #endregion Step One
+
+            #region Step Two
+            List<Dog> allDogs = new List<Dog>()
+            {
+                new Dog { Name = "Little Helpe", Age = 3, Breed = "Golden Retreiver" },
+                new Dog { Name = "Rudolf", Age = 4, Breed = "Bulldog" },
+            };
+            foreach (var dog in allDogs)
+            {
+                Console.WriteLine(dog.Introduction());
+            }
+            #endregion Step Two
+        }
+
+
+    }
+}
