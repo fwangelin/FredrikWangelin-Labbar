008dc4248d35ef62db7d0294d3f9014a244b66fb
Fredrik Wängelin <f.wangelin@gmail.com>
2016-10-17 22:37:27 +0200

    further tries to fix stuff

--- Labb2/FredrikWängelinLabb2/Lab2InherencePolymorphism/Program.cs
+++ Labb2/FredrikWängelinLabb2/Lab2InherencePolymorphism/Program.cs
@@ -13,5 +13,11 @@ namespace Lab2InherencePolymorphism
             Runtime runtime = new Runtime();
             runtime.Start();
         }
+
+        //Hade gärna kört några dagar till på denna, men tyvärr så måste jag lämna in ett väldigt trasigt program. 
+        // Jag har med stor sannolikhet inte löst det enligt vad vi har lärt oss, men kunde inte få till det, tyvärr
+        // Så ett program fullt av buggar är vad jag levererar. 
+        // Ber så mycket om ursäkt för detta.
+       
     }
 }
